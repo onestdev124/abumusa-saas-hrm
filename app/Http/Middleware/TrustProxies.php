48fed115232b3e9aea5722398ec3f60bc239afa4
Hanif Chowdhury <onestdev103@gmail.com>
2024-08-19 16:07:39 +0600

    add IP in TrustProxies middlware

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array|string|null
      */
-    protected $proxies;
+    protected $proxies = ['91.229.245.178'];
 
     // how many trust $proxies
      
